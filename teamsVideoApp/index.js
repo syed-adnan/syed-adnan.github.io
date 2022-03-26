7515889fe306b0685628ba1c42f6a50d8eae40be
Syed Adnan Ahmed <syahme@microsoft..com>
2022-03-25 23:52:30 -0700

    updates

--- teamsVideoApp/index.js
+++ teamsVideoApp/index.js
@@ -1,6 +1,3 @@
-
-// microsoftTeams.initialize(() => {});
-
 // This is the effect for processing
 let appliedEffect = {
   pixelValue: 100,
@@ -13,14 +10,14 @@ let uiSelectedEffect = {};
 let errorOccurs = false;
 //Sample video effect
 function videoFrameHandler(videoFrame, notifyVideoProcessed, notifyError) {
-  // const maxLen =
-  //   (videoFrame.height * videoFrame.width) /
-  //     Math.max(1, appliedEffect.proportion) - 4;
+  const maxLen =
+    (videoFrame.height * videoFrame.width) /
+      Math.max(1, appliedEffect.proportion) - 4;
 
-  // for (let i = 1; i < maxLen; i += 4) {
-  //   //smaple effect just change the value to 100, which effect some pixel value of video frame
-  //   videoFrame.data[i + 1] = appliedEffect.pixelValue;
-  // }
+  for (let i = 1; i < maxLen; i += 4) {
+    //smaple effect just change the value to 100, which effect some pixel value of video frame
+    videoFrame.data[i + 1] = appliedEffect.pixelValue;
+  }
 
   //send notification the effect processing is finshed.
   notifyVideoProcessed();
@@ -33,28 +30,28 @@ function videoFrameHandler(videoFrame, notifyVideoProcessed, notifyError) {
 
 function effectParameterChanged(effectName) {
   console.log(effectName);
-  // if (effectName === undefined) {
-  //   // If effectName is undefined, then apply the effect selected in the UI
-  //   appliedEffect = {
-  //     ...appliedEffect,
-  //     ...uiSelectedEffect,
-  //   };
-  // } else {
-  //   if (effectName === "c4a3ccab-5030-4af9-9c6d-7e0cbab46502") {
-  //     appliedEffect.proportion = 2;
-  //     appliedEffect.pixelValue = 200;
-  //   } else {
-  //     // if effectName is string sent from Teams client, the apply the effectName
-  //     try {
-  //       appliedEffect = {
-  //         ...appliedEffect,
-  //         ...JSON.parse(effectName),
-  //       };
-  //     } catch (e) {
-  //         console.log(e);
-  //     }
-  //   }
-  // }
+  if (effectName === undefined) {
+    // If effectName is undefined, then apply the effect selected in the UI
+    appliedEffect = {
+      ...appliedEffect,
+      ...uiSelectedEffect,
+    };
+  } else {
+    if (effectName === "c4a3ccab-5030-4af9-9c6d-7e0cbab46502") {
+      appliedEffect.proportion = 2;
+      appliedEffect.pixelValue = 200;
+    } else {
+      // if effectName is string sent from Teams client, the apply the effectName
+      try {
+        appliedEffect = {
+          ...appliedEffect,
+          ...JSON.parse(effectName),
+        };
+      } catch (e) {
+          console.log(e);
+      }
+    }
+  }
 }
 
 function previewStatus(arg) {
@@ -76,7 +73,7 @@ window.onload = () => {
         microsoftTeams.video.registerForVideoFrame(videoFrameHandler, {
             format: "NV12"
           });
-      }, 10000);
+      }, 3000);
       
   
 
